460baf327249c568e5d3de1e8ade981d99ed732c
Connor Harris <connorharris@Connors-MacBook-Pro-2.local>
2022-11-01 14:08:24 -0400

    mvp

--- src/App.js
+++ src/App.js
@@ -1,25 +1,94 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+let j = -1;
+
+class App extends Component {
+  
+  render() {
+    const nextClick = () => {
+      j++
+      if (j > 99) {
+        j = 0
+      }
+      document.getElementById("counter").innerHTML = `${j + 1}/203`
+      fetch("https://covid-19-api-production-91c7.up.railway.app/covid")
+        .then((res) => res.json())
+        .then((res) => {
+          let countryArr = [];
+          for (let i in res) {
+            countryArr.push(res[i]);
+          }
+          document.querySelector("#CountryName").innerHTML = countryArr[j].country;
+          document.querySelector("#Population").innerHTML = `Population: ${countryArr[j].population}`;
+          document.querySelector("#CovidCases").innerHTML = `Covid Cases: ${countryArr[j].confirmed}`;
+          document.querySelector("#Deaths").innerHTML = `Covid Deaths: ${countryArr[j].deaths}`;
+          document.querySelector("#LifeExpectancy").innerHTML = `Life Expectancy: ${countryArr[j].life_expectancy
+          }`;
+          console.log(countryArr[j])
+        })
+    };
+
+    const backClick = () => {
+      j--
+      if (j < 0) {
+        j = 99
+      }
+      document.getElementById("counter").innerHTML = `${j + 1}/203`
+      fetch("https://covid-19-api-production-91c7.up.railway.app/covid")
+        .then((res) => res.json())
+        .then((res) => {
+          let countryArr = [];
+          for (let i in res) {
+            countryArr.push(res[i]);
+          }
+          document.querySelector("#CountryName").innerHTML = countryArr[j].country;
+          document.querySelector("#Population").innerHTML = `Population: ${countryArr[j].population}`;
+          document.querySelector("#CovidCases").innerHTML = `Covid Cases: ${countryArr[j].confirmed}`;
+          document.querySelector("#Deaths").innerHTML = `Covid Deaths: ${countryArr[j].deaths}`;
+          document.querySelector("#LifeExpectancy").innerHTML = `Life Expectancy: ${countryArr[j].life_expectancy
+          }`;
+          console.log(countryArr[j])
+        })
+    };
+
+    const randomClick = () => {
+      j = Math.floor(Math.random() * 99)
+      document.getElementById("counter").innerHTML = `${j + 1}/203`
+      fetch("https://covid-19-api-production-91c7.up.railway.app/covid")
+        .then((res) => res.json())
+        .then((res) => {
+          let countryArr = [];
+          for (let i in res) {
+            countryArr.push(res[i]);
+          }
+          document.querySelector("#CountryName").innerHTML = countryArr[j].country;
+          document.querySelector("#Population").innerHTML = `Population: ${countryArr[j].population}`;
+          document.querySelector("#CovidCases").innerHTML = `Covid Cases: ${countryArr[j].confirmed}`;
+          document.querySelector("#Deaths").innerHTML = `Covid Deaths: ${countryArr[j].deaths}`;
+          document.querySelector("#LifeExpectancy").innerHTML = `Life Expectancy: ${countryArr[j].life_expectancy
+          }`;
+          console.log(countryArr[j])
+        })
+    };
+    return (
+      <div className="App">
+        <h1>Country Covid Data</h1>
+        <h2 onClick={nextClick} id="nextButton">Next</h2>
+        <h2 onClick={randomClick} id="randomButton">Random Country</h2>
+        <h2 onClick={backClick} id="backButton">Back</h2>
+        <h3 id="counter"></h3>
+        <div id="statsCont">
+          <h2 id="CountryName" />
+          <h3 id="Population" />
+          <h3 id="CovidCases"/>
+          <h3 id="Deaths" />
+          <h3 id="LifeExpectancy"/>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
