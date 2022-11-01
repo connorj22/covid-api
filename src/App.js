b317e7772807f6bcf35d192279b4a1207975ba93
Connor Harris <connorharris@Connors-MacBook-Pro-2.local>
2022-11-01 14:24:17 -0400

    fixed counter bug

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
   render() {
     const nextClick = () => {
       j++
-      if (j > 99) {
+      if (j > 202) {
         j = 0
       }
       document.getElementById("counter").innerHTML = `${j + 1}/203`
@@ -26,14 +26,14 @@ class App extends Component {
           document.querySelector("#Deaths").innerHTML = `Covid Deaths: ${countryArr[j].deaths}`;
           document.querySelector("#LifeExpectancy").innerHTML = `Life Expectancy: ${countryArr[j].life_expectancy
           }`;
-          console.log(countryArr[j])
+          console.log(j)
         })
     };
 
     const backClick = () => {
       j--
       if (j < 0) {
-        j = 99
+        j = 202
       }
       document.getElementById("counter").innerHTML = `${j + 1}/203`
       fetch("https://covid-19-api-production-91c7.up.railway.app/covid")
@@ -49,34 +49,14 @@ class App extends Component {
           document.querySelector("#Deaths").innerHTML = `Covid Deaths: ${countryArr[j].deaths}`;
           document.querySelector("#LifeExpectancy").innerHTML = `Life Expectancy: ${countryArr[j].life_expectancy
           }`;
-          console.log(countryArr[j])
+          console.log(j)
         })
     };
 
-    const randomClick = () => {
-      j = Math.floor(Math.random() * 99)
-      document.getElementById("counter").innerHTML = `${j + 1}/203`
-      fetch("https://covid-19-api-production-91c7.up.railway.app/covid")
-        .then((res) => res.json())
-        .then((res) => {
-          let countryArr = [];
-          for (let i in res) {
-            countryArr.push(res[i]);
-          }
-          document.querySelector("#CountryName").innerHTML = countryArr[j].country;
-          document.querySelector("#Population").innerHTML = `Population: ${countryArr[j].population}`;
-          document.querySelector("#CovidCases").innerHTML = `Covid Cases: ${countryArr[j].confirmed}`;
-          document.querySelector("#Deaths").innerHTML = `Covid Deaths: ${countryArr[j].deaths}`;
-          document.querySelector("#LifeExpectancy").innerHTML = `Life Expectancy: ${countryArr[j].life_expectancy
-          }`;
-          console.log(countryArr[j])
-        })
-    };
     return (
       <div className="App">
         <h1>Country Covid Data</h1>
         <h2 onClick={nextClick} id="nextButton">Next</h2>
-        <h2 onClick={randomClick} id="randomButton">Random Country</h2>
         <h2 onClick={backClick} id="backButton">Back</h2>
         <h3 id="counter"></h3>
         <div id="statsCont">
